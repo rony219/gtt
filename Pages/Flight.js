6c084ff6de54366f69c6f1b79768e5a1be2e249d
Abir374 <sahaa1873@gmail.com>
2024-09-08 00:44:47 +0530

    add flight and hotel

---
+++ Pages/Flight.js
@@ -0,0 +1,5 @@
+// You can add form validation or other JS features here
+document.querySelector('.form-container').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Search submitted!');
+});
